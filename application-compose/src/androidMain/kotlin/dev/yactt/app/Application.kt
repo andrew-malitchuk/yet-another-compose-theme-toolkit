0d3cf3d7f95bcb2cba9624c61e1f92d1d82eb8b4
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-09-01 20:22:48 +0300

    feat: add initial application structure with main activity and launcher icons

---
+++ application-compose/src/androidMain/kotlin/dev/yactt/app/Application.kt
@@ -0,0 +1,14 @@
+package dev.yactt.app
+
+import android.app.Application
+import dev.yactt.app.di.initKoin
+import org.koin.android.ext.koin.androidContext
+
+class Application : Application(){
+    override fun onCreate() {
+        super.onCreate()
+        initKoin{
+            androidContext(this@Application)
+        }
+    }
+}
