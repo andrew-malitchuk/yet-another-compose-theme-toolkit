c3b340fd7888dcff77808b5e5cea60291e191ca9
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-09-15 20:26:59 +0300

    feat: implement PlatformLogger for Android, JVM, and WASM/JS with Koin DI setup

---
+++ presentation-core-logger-impl/build.gradle.kts
@@ -0,0 +1,20 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    id("dev.yactt.convention.library")
+    id("dev.yactt.convention.di")
+}
+
+kotlin {
+    sourceSets {
+        commonMain.dependencies {
+            implementation(projects.presentationCoreLogger)
+        }
+        androidMain.dependencies {
+        }
+        iosMain.dependencies {
+        }
+        jvmMain.dependencies {
+        }
+    }
+}
