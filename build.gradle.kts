56bc039077cb9e36eef7a4582df8f58783f69163
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-10-13 20:34:13 +0300

    feat: update build configuration and include new domain and presentation features

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     alias(libs.plugins.composeMultiplatform) apply false
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
-    kotlin("plugin.serialization") version "2.0.20"
+    alias(libs.plugins.kotlinSerialization) apply false
 }
 
 buildscript {
